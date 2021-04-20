e729196e56bdd2eeb1aaa59dc26fa47a6dd942da
Guilherme Turtera <guiturtera@hotmail.com>
2021-04-19 22:39:33 -0300

    Setup ready for list development

---
+++ src/Linear-data-struct/List/IList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_data_struct
+{
+    public interface IList<T>
+    {
+        int Count { get; }
+        void Add(T value);
+        void Clear();
+        bool Remove(T data);
+        void RemoveAt(int index);
+        void Insert(T value, int index);
+        T GetTop();
+        T GetBottom();
+        T GetAtt(int index);
+    }
+    
+}
---
+++ src/Linear-data-struct/List/StaticList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_data_struct
+{
+    public class StaticList<T> : IList<T>
+    {
+        public int Count => throw new NotImplementedException();
+
+        public void Add(T value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetAtt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetBottom()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetTop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T value, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
