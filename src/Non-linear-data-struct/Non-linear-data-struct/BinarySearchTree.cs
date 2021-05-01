aac10e2960706ee5d56ba003c205179d1ed568b8
Guilherme Turtera <guiturtera@hotmail.com>
2021-05-01 19:46:38 -0300

    Binary tree added with tests

--- src/Non-linear-data-struct/Non-linear-data-struct/BinarySearchTree.cs
+++ src/Non-linear-data-struct/Non-linear-data-struct/BinarySearchTree.cs
@@ -10,7 +10,6 @@ namespace Non_linear_data_struct
 {
     // This SearchTree is only an Concept Example. 
     // When searching for some data, we usually use Self-Balanced trees, such as AVL ones.
-    // If they are not in the project yet, it'll be in a few days
     // It will be done only to simplify, with INTEGER.
     public class BinarySearchTree
     {
@@ -43,6 +42,204 @@ namespace Non_linear_data_struct
                 else
                     parentCurrentNode.Right = nodeToCreate;
             }
+        }
+
+        // Search for a node and returns if the value exists.
+        // Same time complexity as the Insert, since also searches for a node
+        public bool Contains(int value) 
+        {
+            return Search(value).Exists;
+        }
+
+        // THIS METHOD WILL DELETE A VALUE FROM THE TREE
+        // IT HAS O(logN) in average, and O(N) in the worst (Only one path)
+        public bool Delete(int value) 
+        {
+            SearchResponse response = Search(value);
+            Node parent = response.FoundParent;
+            Node found = response.Found;
+
+            if (response.Exists)
+            {
+                // LEAF NODE
+                if (found.Left == null && found.Right == null)
+                {
+                    // IF IT'S THE ROOT NODE
+                    if (parent == null)
+                        root = null;
+                    else
+                        DeleteParentRef(parent, found);
+                }
+                // NODE HAS TWO CHILDREN
+                else if (found.Left != null && found.Right != null)
+                {
+                    Node replaceNode = found.Right;
+                    Node rightLowerNode = FindLowerNode(found.Right);
+                    rightLowerNode.Left = found.Left;
+                    ReplaceNode(found, replaceNode);
+                }
+                // NODE HAS ONE LEFT CHILD
+                else if (found.Left != null)
+                {
+                    ReplaceNode(found, found.Left);
+                }
+                // NODE HAS ONE RIGHT CHILD
+                else
+                {
+                    ReplaceNode(found, found.Right);
+                }
+            }
+
+            return response.Exists;
+        }
+
+        public int Height()
+        {
+            return Height(root);
+        }
+
+        private int Height(Node currentNode)
+        {
+            int heightLeft, heightRight;
+            if (currentNode == null)
+                return 0;
+            else
+            {
+                heightLeft = Height(currentNode.Left);
+                heightRight = Height(currentNode.Right);
+                if (heightLeft > heightRight)
+                    return heightLeft + 1;
+                else
+                    return heightRight + 1;
+            }
+        }
+
+        // ROOT - LEFT - RIGHT
+        public List<int> PreOrderTransversal()
+        {
+            List<int> list = new List<int>();
+            PreOrderTransversal(list, root);
+            return list;
+        }
+
+        private void PreOrderTransversal(List<int> list, Node currentNode) 
+        {
+            if (currentNode != null)
+            {
+                list.Add(currentNode.Value);
+                PreOrderTransversal(list, currentNode.Left);
+                PreOrderTransversal(list, currentNode.Right);
+            }
+        }
+
+        // LEFT - ROOT - RIGHT
+        public List<int> InOrderTransversal()
+        {
+            List<int> list = new List<int>();
+            InOrderTransversal(list, root);
+            return list;
+        }
+
+        private void InOrderTransversal(List<int> list, Node currentNode)
+        {
+            if (currentNode != null)
+            {
+                InOrderTransversal(list, currentNode.Left);
+                list.Add(currentNode.Value);
+                InOrderTransversal(list, currentNode.Right); 
+            }
+        }
+
+        // LEFT - RIGHT - ROOT 
+        public List<int> PostOrderTransversal()
+        {
+            List<int> list = new List<int>();
+            PostOrderTransversal(list, root);
+            return list;
+        }
+
+        private void PostOrderTransversal(List<int> list, Node currentNode)
+        {
+            if (currentNode != null)
+            {
+                PostOrderTransversal(list, currentNode.Left);
+                PostOrderTransversal(list, currentNode.Right);
+                list.Add(currentNode.Value);
+            }
+        }
+
+        private void ReplaceNode(Node currentNode, Node newNode) 
+        {
+            currentNode.Left = newNode.Left;
+            currentNode.Right = newNode.Right;
+            currentNode.Value = newNode.Value;
+        }
+
+        public Node FindLowerNode()
+        {
+            return FindLowerNode(root);
+        }
+
+        private Node FindLowerNode(Node root)
+        {
+            if (root.Left == null)
+                return root;
+
+            return FindLowerNode(root.Left);
+        }
+
+        public Node FindHigherNode()
+        {
+            return FindHigherNode(root);
+        }
+
+        private Node FindHigherNode(Node root)
+        {
+            if (root.Right == null)
+                return root;
+
+            return FindHigherNode(root.Right);
+        }
+
+        private void DeleteParentRef(Node parent, Node found)
+        {
+            if (found.Value < parent.Value)
+                parent.Left = null;
+            else
+                parent.Right = null;
+        }
+
+        private SearchResponse Search(int value) 
+        {
+            Node parentNode = null;
+            Node currentNode = root;
+            while (currentNode != null)
+            {
+                if (currentNode.Value == value)
+                    return new SearchResponse(true, currentNode, parentNode);
+
+                parentNode = currentNode;
+                if (value > currentNode.Value)
+                    currentNode = currentNode.Right;
+                else
+                    currentNode = currentNode.Left;
+            }
+
+            return new SearchResponse(false, null, null);
+        }
+    }
+
+    internal class SearchResponse 
+    {
+        public Node FoundParent { get; set; }
+        public Node Found { get; set; }
+        public bool Exists { get; set; }
+
+        public SearchResponse(bool exists, Node found, Node foundParent) 
+        {
+            this.Exists = exists;
+            this.Found = found;
+            this.FoundParent = foundParent;
         }
     }
 }
--- src/Non-linear-data-struct/Test_Non-linear-data-struct/Binary_Tree_Tests.cs
+++ src/Non-linear-data-struct/Test_Non-linear-data-struct/Binary_Tree_Tests.cs
@@ -1,51 +1,148 @@
 using NUnit.Framework;
 using Non_linear_data_struct;
+using System.Collections.Generic;
 
 namespace Test_Non_linear_data_struct
 {
     public class Binary_Tree_Tests
     {
+        BinarySearchTree test;
         [SetUp]
         public void Setup()
         {
+            test = new BinarySearchTree();
+            test.Insert(1);
+            test.Insert(2);
+            test.Insert(4);
+            test.Insert(0);
+            test.Insert(-5);
+            test.Insert(10);
+            test.Insert(6);
+            test.Insert(5);
+            test.Insert(3);
         }
 
         [Test]
         public void Insert_Test()
         {
-            BinarySearchTree simple = new BinarySearchTree();
-            simple.Insert(2);
-            Assert.AreEqual(2, simple.root.Value);
-            Assert.AreEqual(null, simple.root.Right);
-            Assert.AreEqual(null, simple.root.Left);
-            simple.Insert(5);
-            Assert.AreEqual(2, simple.root.Value);
-            Assert.AreEqual(5, simple.root.Right.Value);
-            Assert.AreEqual(null, simple.root.Left);
-            simple.Insert(6);
-            Assert.AreEqual(2, simple.root.Value);
-            Assert.AreEqual(6, simple.root.Right.Right.Value);
-            Assert.AreEqual(null, simple.root.Left);
-            simple.Insert(4);
-            Assert.AreEqual(2, simple.root.Value);
-            Assert.AreEqual(4, simple.root.Right.Left.Value);
-            Assert.AreEqual(null, simple.root.Left);
-            simple.Insert(1);
-            Assert.AreEqual(2, simple.root.Value);
-            Assert.AreEqual(4, simple.root.Right.Left.Value);
-            Assert.AreEqual(1, simple.root.Left.Value);
-            simple.Insert(-1);
-            Assert.AreEqual(2, simple.root.Value);
-            Assert.AreEqual(4, simple.root.Right.Left.Value);
-            Assert.AreEqual(-1, simple.root.Left.Left.Value);
-            simple.Insert(0);
-            Assert.AreEqual(2, simple.root.Value);
-            Assert.AreEqual(4, simple.root.Right.Left.Value);
-            Assert.AreEqual(0, simple.root.Left.Left.Right.Value);
-            simple.Insert(3);
-            Assert.AreEqual(2, simple.root.Value);
-            Assert.AreEqual(5, simple.root.Right.Value);
-            Assert.AreEqual(3, simple.root.Right.Left.Left.Value);
+            BinarySearchTree tree = new BinarySearchTree();
+            tree.Insert(2);
+            Assert.AreEqual(2, tree.root.Value);
+            Assert.AreEqual(null, tree.root.Right);
+            Assert.AreEqual(null, tree.root.Left);
+            tree.Insert(5);
+            Assert.AreEqual(2, tree.root.Value);
+            Assert.AreEqual(5, tree.root.Right.Value);
+            Assert.AreEqual(null, tree.root.Left);
+            tree.Insert(6);
+            Assert.AreEqual(2, tree.root.Value);
+            Assert.AreEqual(6, tree.root.Right.Right.Value);
+            Assert.AreEqual(null, tree.root.Left);
+            tree.Insert(4);
+            Assert.AreEqual(2, tree.root.Value);
+            Assert.AreEqual(4, tree.root.Right.Left.Value);
+            Assert.AreEqual(null, tree.root.Left);
+            tree.Insert(1);
+            Assert.AreEqual(2, tree.root.Value);
+            Assert.AreEqual(4, tree.root.Right.Left.Value);
+            Assert.AreEqual(1, tree.root.Left.Value);
+            tree.Insert(-1);
+            Assert.AreEqual(2, tree.root.Value);
+            Assert.AreEqual(4, tree.root.Right.Left.Value);
+            Assert.AreEqual(-1, tree.root.Left.Left.Value);
+            tree.Insert(0);
+            Assert.AreEqual(2, tree.root.Value);
+            Assert.AreEqual(4, tree.root.Right.Left.Value);
+            Assert.AreEqual(0, tree.root.Left.Left.Right.Value);
+            tree.Insert(3);
+            Assert.AreEqual(2, tree.root.Value);
+            Assert.AreEqual(5, tree.root.Right.Value);
+            Assert.AreEqual(3, tree.root.Right.Left.Left.Value);
         }
+
+        [Test]
+        public void Contains_Test()
+        {
+            Assert.AreEqual(false, test.Contains(50));
+            Assert.AreEqual(false, test.Contains(-50));
+            Assert.AreEqual(true, test.Contains(1));
+            Assert.AreEqual(true, test.Contains(2));
+            Assert.AreEqual(true, test.Contains(3));
+            Assert.AreEqual(true, test.Contains(0));
+            Assert.AreEqual(true, test.Contains(-5));
+            Assert.AreEqual(true, test.Contains(10));
+            Assert.AreEqual(true, test.Contains(6));
+            Assert.AreEqual(true, test.Contains(5));
+            Assert.AreEqual(false, test.Contains(20));
+            Assert.AreEqual(false, test.Contains(-20));
+        }
+
+        [Test]
+        public void Delete_Test() 
+        {
+            AssertDeletedNode(2, true);
+            AssertDeletedNode(5, true);
+            AssertDeletedNode(200, false);
+            AssertDeletedNode(1, true);
+            AssertDeletedNode(2, false);
+            AssertDeletedNode(10, true);
+            AssertDeletedNode(6, true);
+            AssertDeletedNode(0, true);
+            AssertDeletedNode(3, true);
+            AssertDeletedNode(-5, true);
+            AssertDeletedNode(0, false);
+            AssertDeletedNode(4, true);
+            Assert.AreEqual(true, test.root == null);
+        }
+
+        [Test]
+        public void Height_Test()
+        {
+            Assert.AreEqual(6, test.Height());
+        }
+
+        private void AssertDeletedNode(int value, bool exists) 
+        {
+            Assert.AreEqual(exists, test.Contains(value));
+            Assert.AreEqual(exists, test.Delete(value));
+            Assert.AreEqual(false, test.Contains(value));
+        }
+
+        [Test]
+        public void FindLowerNode_Test() 
+        {
+            Assert.AreEqual(-5, test.FindLowerNode().Value);
+        }
+
+        [Test]
+        public void FindHigherNode_Test()
+        {
+            Assert.AreEqual(10, test.FindHigherNode().Value);
+        }
+
+        [Test]
+        public void PreOrderTransversal_Test()
+        {
+            List<int> listPreOrder = new List<int>() { 1, 0, -5, 2, 4, 3, 10, 6, 5 };
+            List<int> result = test.PreOrderTransversal();
+            Assert.AreEqual(listPreOrder, result);
+        }
+
+        [Test]
+        public void InOrderTransversal_Test()
+        {
+            List<int> listInOrder = new List<int>() { -5, 0, 1, 2, 3, 4, 5, 6, 10 };
+            List<int> result = test.InOrderTransversal();
+            Assert.AreEqual(listInOrder, result);
+        }
+
+        [Test]
+        public void PostOrderTransversal_Test() 
+        {
+            List<int> listPostOrder = new List<int>() { -5, 0, 3, 5, 6, 10, 4, 2, 1 };
+            List<int> result = test.PostOrderTransversal();
+            Assert.AreEqual(listPostOrder, result);
+        }
+
     }
 }
